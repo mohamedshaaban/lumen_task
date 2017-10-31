ec81092a82849c54372cbf442248a2fdd3d2c1df
unknown <mohammad.shabaan@gmail.com>
2017-11-01 00:41:17 +0200

    database migrations

---
+++ database/migrations/2017_10_29_093609_create_order_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('order_id');
+            $table->string('email');
+            $table->double('total_amount_net');
+            $table->double('shipping_costs');
+            $table->string('payment_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
